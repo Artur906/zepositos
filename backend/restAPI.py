e141a9d4c922bd694509408c11661b1e206e1114
André Alves Correia <andre2013aguabranca@gmail.com>
2022-12-01 14:14:00 -0300

    added get(by id), put and delete (cliente)

--- backend/restAPI.py
+++ backend/restAPI.py
@@ -46,9 +46,79 @@ def get_clientes():
     return res
 
 
+
+#GET
+@app.route('/clientes/<id>')
+def get_cliente(id):
+    try:
+        cliente = Cliente.get_by_id(id)
+    except:
+        cliente = None
+    if(cliente):
+        res = jsonify(cliente.serialize)
+        res.status_code = 200
+    else:
+        output = {
+            "error": "Sem resultados encontrados. Cheque a URL e tente outra vez.",
+            "url": request.url
+        }
+        res = jsonify(output)
+        res.status_code = 404
+    return res
+
+
+
 #POST
 @app.route('/clientes', methods=['POST'])
 def add_cliente():
     cliente = Cliente(nome=request.json['nome'], telefone=request.json['telefone'])
     cliente.save()
     return {'id':cliente.get_id()}
+
+
+
+
+#PUT
+@app.route('/clientes/<id>', methods=['PUT'])
+def update_cliente(id):
+    if (type(request.json['nome']) != str) or (type(request.json['telefone'])):
+        abort(400)
+    try:
+        cliente = Cliente.get_by_id(id)
+    except:
+        cliente = None
+
+    if cliente:
+        cliente.nome = request.json['nome']
+        cliente.telefone = request.json['telefone']
+        cliente.save()
+        res = jsonify({"message": "Atualizado!"})
+        res.status_code = 200
+    else:
+        output = {
+            "error": "Não encontrado.",
+            "url": request.url
+        }
+        res = jsonify(output)
+        res.status_code = 404
+    return res
+
+
+#DELETE
+@app.route('/clientes/<id>', methods=['DELETE'])
+def delete_cliente(id):
+    cliente = Cliente.select().where(Cliente.id == id)[0]
+    if(cliente):
+        cliente.delete_instance()
+        res = jsonify({"message": "Deletado!"})
+        res.status_code = 200
+    else:
+        output = {
+            "error": "Não encontrado.",
+            "url": request.url
+        }
+        res = jsonify(output)
+        res.status_code = 404
+    return res
+
+
