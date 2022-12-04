375c729c5c7c3ee95bc49dee6891a0c2e797b4a1
Meljael Daniel <73859563+meljael@users.noreply.github.com>
2022-12-04 19:54:10 -0300

    criei a lista-embarques

--- frontend/main.js
+++ frontend/main.js
@@ -3,7 +3,10 @@ import './public/styles/style.css'
 document.querySelector('#app').innerHTML = `
   <div>
     <h2> páginas </h2>
+    
     <a href="src/cadastro-cliente.html">cadastro cliente</a>
     <a href="src/manipular-cliente.html">manipular cliente</a>
+    <a href="src/embarque.html">embarque</a>
+    <a href="src/lista-embarques.html">lista de embarques</a>
   </div>
 `
