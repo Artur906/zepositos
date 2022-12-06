db632d8ff1340044a24fb9239eba58b64210719d
André Alves Correia <andre2013aguabranca@gmail.com>
2022-12-06 16:30:20 -0300

    separation of the html, it's style and it's script

---
+++ frontend/src/cadastro-cliente/cadastro-cliente-script.js
@@ -0,0 +1,58 @@
+
+import axios from 'axios' //node
+import { textMarshal } from 'text-marshal' // node
+import { BASE_URL_API } from '../variaveisAmbiente.js'
+
+
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(form)
+
+    const data = {
+        nome: formData.get('nome'),
+        telefone: formData.get('telefone'),
+    }
+
+    document.querySelector('#status').innerHTML =
+        `<div class="spinner-border" role="status">
+            <span class="sr-only"></span>
+        </div>`
+
+    axios.post(`${BASE_URL_API}/clientes`, data)
+        .then(res => {
+            document.querySelector('#status').innerHTML =
+                `<div class="alert alert-success" role="alert">
+                Cliente Cadastrado!
+            </div>`
+        })
+        .catch(err => {
+            document.querySelector('#status').innerHTML =
+                `<div class="alert alert-danger" role="alert">
+                Não foi possível cadastrar o cliente!
+            </div>`
+        })
+        .then(res => {
+            setTimeout(() => window.location.href = "./cadastro-cliente.html", 5000)
+        })
+})
+
+
+
+form.addEventListener('reset', function (e) {
+    window.location.href = "../lista-embarques.html"
+})
+
+const cardnumber = document.getElementById('telefone');
+
+cardnumber.oninput = function (e) {
+    let data = textMarshal({
+        input: e.target.value,
+        template: cardnumber.getAttribute('data-pattern'),
+        disallowCharacters: [/[a-z]/],
+    });
+
+    cardnumber.value = data.marshaltext;
+};
