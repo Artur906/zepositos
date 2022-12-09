72194163833c87ee2711fea3cf61ce893a3c6a14
André Alves Correia <andre2013aguabranca@gmail.com>
2022-12-09 08:42:02 -0300

    ClientesList get and post finished

--- backend/src/controllers/clientes.py
+++ backend/src/controllers/clientes.py
@@ -1,49 +1,91 @@
-from flask_restx import Resource
+from flask_restx import Resource, abort
 from flask import jsonify
 from flask import request
 
-from server.postgredb import *
-from server.instance import server
-from models.clientes import cliente
+from src.server.postgredb import *
+from src.server.instance import server
+from src.models.clientes import cliente
 app, api = server.app, server.api
 
 ITEM_NOT_FOUND = 'Cliente não encontrado'
 
+
+
 @api.route('/clientes')
-class ClientesEndPoints(Resource):
-    @api.marshal_list_with(cliente)
+class ClientesList(Resource):
+
+    @api.doc('get_clientes')
+    @api.marshal_list_with(cliente, envelope="clientes", code=200)
+    @api.doc(responses={404: 'Sem clientes registrados.'})
     def get(self):
-        
         query = Cliente.select()
         try:
             dados = [i.serialize for i in query]
         except:
             dados = None
-
         if dados:
-            res = {'clientes': dados}
-            return res, 200
+            return dados, 200
         else:
-            res = jsonify(error = ITEM_NOT_FOUND, url = request.url)
-            res.status_code = 404
+            abort(404, 'Sem clientes registrados.')
 
-        return res
-    
+
+    @api.doc('create_cliente')
     @api.expect(cliente, validate=True)
-    @api.marshal_with(cliente)
+    @api.marshal_with(cliente, code=201)
+    @api.doc(responses={404: 'Campo id é de leitura apenas.'})
     def post(self):
-        response = api.payload
+        payload = api.payload
 
-        '''try:
-            response['nome']
+        payloadHasIdField = True
+        try:
+            payload['id']
+            
         except:
-            res = {"message":"Campo obrigatório (nome) em falta."}
-            return res, 400'''
-        
-        
-        cliente = Cliente(**response)
-        cliente.save()
-        res = {"message": 'Cliente criado.', "id": cliente.get_id()}
-        return res, 201
+            payloadHasIdField = False
 
-    
+        if(payloadHasIdField):
+            abort(400, "Campo id é de leitura apenas.")
+
+        cliente = Cliente(**payload)
+        try:
+            cliente.save()
+            return cliente.serialize, 201
+        except Exception as e:
+            abort(400, e)
+
+        
+
+
+@api.route('/clientes/<int:id>')
+class Clientes(Resource):
+
+    @api.marshal_with(cliente, code=200)
+    @api.doc(responses={404: ITEM_NOT_FOUND})
+    def get(self, id):
+        try:
+            cliente = Cliente.get_by_id(id)
+        except:
+            cliente = None
+        if(cliente):
+            return cliente.serialize, 200
+            
+        else:
+            abort(404, ITEM_NOT_FOUND)
+
+
+    #to do
+    def patch(self, id):
+        payload = api.payload
+        try:
+            cliente = Cliente.get_by_id(id)
+        except:
+            cliente = None
+
+        if cliente:
+            Cliente.update(**payload).where(Cliente.id == id).execute()
+            updatedCliente = Cliente.get_by_id(id)
+            return updatedCliente.serialize, 200
+            
+        else:
+            abort(404, ITEM_NOT_FOUND)
+        
