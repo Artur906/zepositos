8e6749991a94681a6a55ec071be8d5641b90812d
Artur Dantas <arturdantasrodrigues@gmail.com>
2022-12-08 23:56:11 -0300

    requisições com axios, listando embarques de clientes no modal

--- frontend/src/listar-clientes/listar-clientes-script.js
+++ frontend/src/listar-clientes/listar-clientes-script.js
@@ -1,55 +1,69 @@
-// import axios from 'axios'
-// import {BASE_URL_API} from '../variaveisAmbiente'
+import axios from 'axios'
+import { BASE_URL_API } from '../variaveisAmbiente'
 
-const dados = { clientes: [
-    {
-        nome: 'maria', 
-        quant_embarques: 0,
-        id: 1
-    },
-    {
-        nome: 'JOse', 
-        quant_embarques: 0,
-        id: 2
-    },
-    {
-        nome: 'mGuiador', 
-        quant_embarques: 0,
-        id: 3
-    },
-    {
-        nome: 'maria4', 
-        quant_embarques: 0,
-        id: 4
-    }
-]}
-const tabela = document.querySelector('.table-body')
+const tabelaClientes = document.querySelector('.table-clientes')
 
-dados.clientes.forEach(cliente => { 
-    let table_row = 
+
+//requisições
+async function pegarClientes() {
+    const clientes = (await axios.get(`${BASE_URL_API}/clientes`)).data
+
+    return clientes
+}
+
+async function pegarEmbarquesCliente(idCliente) {
+    const embarques = (await axios.get(`${BASE_URL_API}/clientes/${idCliente}/embarques`)).data
+
+    return embarques
+}
+
+
+let dadosClientes = await pegarClientes()
+
+dadosClientes.clientes.forEach(cliente => {
+    let table_row =
         ` 
             <tr id_cliente="${cliente.id}" data-toggle="modal" data-target="#exampleModalCenter">
                 <td>${cliente.nome}</td>
                 <td>${cliente.quant_embarques}</td>
             </tr> 
         `
-    tabela.innerHTML += table_row;
+    tabelaClientes.innerHTML += table_row;
 });
 
-const linhas = document.querySelectorAll('.table-body tr')
+const linhasTableClientes = document.querySelectorAll('.table-clientes tr')
 
-linhas.forEach(linha => {
+linhasTableClientes.forEach(linha => {
     linha.addEventListener('click', () => {
         let idCliente = linha.getAttribute("id_cliente")
-        modalEmbarques(idCliente)
+        const cliente = dadosClientes.clientes.find(cliente => { return cliente.id == idCliente })
+        modalEmbarques(cliente)
     })
 })
 
-// vai colocar os elementos na modal
-function modalEmbarques(clienteId) {
-    const cliente = dados.clientes.find(cliente => {return cliente.id == clienteId})
-    document.querySelector('.modal-title').textContent = cliente.nome
-    document.querySelector('.modal-body').innerHTML = "aaaaaaaaaaaaaaaaaa eu to vivoooooo"
-}
 
-console.log(id)
+// vai colocar os elementos na modal
+async function modalEmbarques(cliente) {
+    document.querySelector('.modal-title').textContent = `Embarques de ${cliente.nome}`
+    const tabelaEmbarques = document.querySelector('.table-embarques')
+
+
+    const dadosEmbarques = await pegarEmbarquesCliente(cliente.id)
+    console.log(dadosEmbarques)
+
+    dadosEmbarques.embarques.forEach(embarque => {
+        let table_row =
+            ` 
+                <tr embarque_id = "${embarque.id}">
+                    <td>${embarque.descricao}</td>
+                    <td>${embarque.quant_volumes}</td>
+                    <td>${embarque.peso_total}</td>
+                    <td>${embarque.com_nota_fiscal ? "sim" : "não"}</td>
+                    <td>${embarque.data_chegada}</td>
+                </tr>
+            `
+        tabelaEmbarques.innerHTML += table_row;
+    });
+
+    const linhasTabelaEmbarques = document.querySelectorAll('.table-embarques tr')
+}
