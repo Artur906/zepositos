59e9b8510bc57b4a209fe0cb0cc0cbd6e89e49aa
André Alves Correia <andre2013aguabranca@gmail.com>
2022-12-09 08:42:46 -0300

    code comment edited

--- backend/src/server/esquemadb.py
+++ backend/src/server/esquemadb.py
@@ -1,4 +1,3 @@
-
 from peewee import Model, TextField,ForeignKeyField, BooleanField, DateField, DecimalField, IntegerField
 from datetime import datetime
 
@@ -13,7 +12,7 @@ VOLUME(id, id_embarque, largura, comprimento, altura, peso)
 class BaseModel(Model):
     @property
     def serialize(self):# to-do: make it abstract, so children classes MUST implement it.
-        # MUST return self in dict format. ex: {'key1': self.value1, 'key2': self.value2, ...}
+        # MUST return self in dict (json) format. ex: {'key1': self.value1, 'key2': self.value2, ...}
         pass
    
 
