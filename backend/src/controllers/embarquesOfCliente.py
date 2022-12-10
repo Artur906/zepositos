ff85dad868525cd26724ed0dd77ed447e485e967
André Alves Correia <andre2013aguabranca@gmail.com>
2022-12-09 21:00:46 -0300

    criadas rotas customizadas
    /clientes/id/embarques
     e
    /embarques/id/volumes

---
+++ backend/src/controllers/embarquesOfCliente.py
@@ -0,0 +1,27 @@
+from flask_restx import Resource, abort
+
+from server.postgredb import *
+from server.instance import server
+from models.embarques import embarque_model
+from controllers.handyFunctions import has_id
+
+
+app, api = server.app, server.api
+
+ITEM_NOT_FOUND = 'Nenhum embarque encontrado'
+
+
+@api.route('/clientes/<int:id_cliente>/embarques')
+class EmbarquesOfCliente(Resource):
+
+    @api.doc('get_embarques_of_cliente')
+    @api.marshal_with(embarque_model, code=200)
+    @api.doc(responses={404: ITEM_NOT_FOUND})
+    def get(self, id_cliente):
+
+        query = Embarque.select().where(Embarque.id_cliente == id_cliente)
+        if(query.count() == 0):
+            abort(404, ITEM_NOT_FOUND)    
+        dados = [i.serialize for i in query]
+        return dados, 200
+        
--- backend/src/main.py
+++ backend/src/main.py
@@ -3,5 +3,7 @@ from server.instance import server
 from controllers.clientes import *
 from controllers.embarques import *
 from controllers.volumes import *
+from controllers.embarquesOfCliente import *
+from controllers.volumesOfEmbarque import *
 
 server.run()
