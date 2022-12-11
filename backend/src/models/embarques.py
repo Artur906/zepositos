f5a21674a5098742637bbfe384b5355dabacab95
André Alves Correia <andre2013aguabranca@gmail.com>
2022-12-10 23:54:11 -0300

    Agora o id_cliente pode ser null !

--- backend/src/models/embarques.py
+++ backend/src/models/embarques.py
@@ -2,9 +2,13 @@ from datetime import date
 from flask_restx import fields
 from server.instance import server
 
+class NullableInteger(fields.Integer):
+    __schema_type__ = ['integer', 'null']
+    __schema_example__ = 'nullable integer'
+
 embarque_model = server.api.model('Embarque', {
     'id':               fields.Integer(readonly=True, description='O identificador do embarque.', example=1),
-    'id_cliente':       fields.Integer(description='O identificador do cliente dono do embarque.', example=1),
+    'id_cliente':       NullableInteger(default=None, description='O identificador do cliente dono do embarque.', example="1"),
     'descricao':        fields.String(required=True, max_length=200, description='A descricao do embarque.', example="Caixas pretas grandes, entrega em domicílio."),
     'data_chegada':     fields.Date(default=date.today(), description='Data em que o embarque chegou ao depósito.'),
     'quant_volumes':    fields.Integer(readonly=True, description='A quantidade de volumes do embarque.', example = 3),
