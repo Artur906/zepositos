e1d8f0184614c63cdf5ea63a0bd71c57f64765a3
Artur Dantas <arturdantasrodrigues@gmail.com>
2022-12-13 17:27:22 -0300

    refatorando post de volumes

--- frontend/src/scripts/cadastro-embarque-script.js
+++ frontend/src/scripts/cadastro-embarque-script.js
@@ -110,11 +110,25 @@ form.addEventListener('submit', function (e) {
     document.querySelector('.btn-salvar').disabled = true
     
     const formData = new FormData(form)
+    const listaVolumes = []
+    let elemento = 1
+
+    do {
+        listaVolumes.push({
+            largura: parseInt(formData.get(`larg${elemento}`)),
+            comprimento: parseInt(formData.get(`comp${elemento}`)),
+            altura: parseInt(formData.get(`alt${elemento}`)),
+            peso: parseFloat(formData.get(`peso${elemento}`))
+        })
+        elemento++
+    } while (formData.get(`comp${elemento}`))
+
     const data = {
         id_cliente: parseInt(formData.get('cliente')),
         descricao: formData.get('descricao'),
         data_chegada: formData.get('data'),
-        com_nota_fiscal: formData.get('nota-fiscal') == null ? false : true
+        com_nota_fiscal: formData.get('nota-fiscal') == null ? false : true, 
+        volumes: listaVolumes
     }
 
     // armazenando o id do cliente para selecioná-lo automáticamente quando a página recarregar
@@ -124,29 +138,6 @@ form.addEventListener('submit', function (e) {
 
     axios.post(`${BASE_URL_API}/embarques`, data)
         .then(res => {
-            const volumes = []
-            let elemento = 1
-
-            do {
-                volumes.push({
-                    id_embarque: res.data.id,
-                    largura: parseInt(formData.get(`larg${elemento}`)),
-                    comprimento: parseInt(formData.get(`comp${elemento}`)),
-                    altura: parseInt(formData.get(`alt${elemento}`)),
-                    peso: parseFloat(formData.get(`peso${elemento}`))
-                })
-                elemento++
-            } while (formData.get(`comp${elemento}`))
-
-            volumes.forEach(volume => {
-                axios.post(`${BASE_URL_API}/volumes`, volume)
-                    .catch(err => {
-                        statusFunctions.failedStatus("Não foi possível cadastrar o embarque (erro no volume)!")
-                        console.log(err.response.data)
-                        return
-                    })
-            })
-
             statusFunctions.sucessStatus("Embarque cadastrado com sucesso!")
         })
         .catch(err => {
