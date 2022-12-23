8e671192edb507d7b72f730db623269a17503654
André Alves Correia <andre2013aguabranca@gmail.com>
2022-12-23 12:35:39 -0300

    exemplo mais realista de volume

--- backend/src/models/volumes.py
+++ backend/src/models/volumes.py
@@ -2,8 +2,8 @@ from flask_restx import fields
 from src.server.instance import server
 
 volume_model = server.api.model('Volume', {
-    'largura':     fields.Integer(required=True, min=1, description='A largura do volume em centímetros.', example=100),
-    'altura':      fields.Integer(required=True, min=1, description='A altura do volume em centímetros.', example=250),
-    'comprimento': fields.Integer(required=True, min=1, description='O comprimento do volume em centímetros.', example=300),
+    'largura':     fields.Integer(required=True, min=1, description='A largura do volume em centímetros.', example=110),
+    'altura':      fields.Integer(required=True, min=1, description='A altura do volume em centímetros.', example=75),
+    'comprimento': fields.Integer(required=True, min=1, description='O comprimento do volume em centímetros.', example=100),
     'peso':        fields.Fixed(required=True, min=0.1, description='O peso do volume em quilogramas.', example=10.5)
 })
